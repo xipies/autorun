eec30a521a055a96a647fbef748f053d5ee8f116
xipies <aishaneko@gmail.com>
2018-04-17 07:43:52 -0700

    Ashita v3 updates

--- src/addons/autorun/autorun.lua
+++ src/addons/autorun/autorun.lua
@@ -1,19 +1,27 @@
 
 _addon.author   = 'Eleven Pies';
 _addon.name     = 'AutoRun';
-_addon.version  = '2.1.0';
+_addon.version  = '3.1.0';
 
 require 'common'
 
 local auto_follow;
 
+local function write_float_hack(addr, value)
+    local packed = struct.pack('f', value);
+    local unpacked = { struct.unpack('B', packed, 1), struct.unpack('B', packed, 2), struct.unpack('B', packed, 3), struct.unpack('B', packed, 4) };
+
+    -- ashita.memory.write_float appears busted in ashita v3, converting to byte array
+    ashita.memory.write_array(addr, unpacked);
+end
+
 local function setAutoRun(value)
     if (value) then
         local entity = AshitaCore:GetDataManager():GetEntity();
         local party = AshitaCore:GetDataManager():GetParty();
-        local selfindex = party:GetPartyMemberTargetIndex(0);
+        local selfindex = party:GetMemberTargetIndex(0);
         local yaw = entity:GetLocalYaw(selfindex);
-        local speed = entity:GetMovementSpeed(selfindex);
+        local speed = entity:GetSpeed(selfindex);
         local status = entity:GetStatus(selfindex);
         -- On chocobo, so double speed
         if (status == 5) then
@@ -22,23 +30,23 @@ local function setAutoRun(value)
         local calc_x = math.cos(yaw) * speed * 0.05 * 0.33333333333333333333333333333333;
         local calc_y = 0;
         local calc_z = 0 - (math.sin(yaw) * speed * 0.05 * 0.33333333333333333333333333333333);
-        mem.WriteFloat(auto_follow + 12, calc_x); -- DirectionX
-        mem.WriteFloat(auto_follow + 16, calc_y); -- DirectionY
-        mem.WriteFloat(auto_follow + 20, calc_z); -- DirectionZ
-        mem.WriteULong(auto_follow + 36, 0x4000000); -- FollowID
-        mem.WriteUChar(auto_follow + 41, 1); -- AutoRun
+        write_float_hack(auto_follow + 12, calc_x); -- DirectionX
+        write_float_hack(auto_follow + 16, calc_y); -- DirectionY
+        write_float_hack(auto_follow + 20, calc_z); -- DirectionZ
+        ashita.memory.write_uint32(auto_follow + 36, 0x4000000); -- FollowID
+        ashita.memory.write_uint8(auto_follow + 41, 1); -- AutoRun
     else
         print('Follow cancelled.');
-        mem.WriteUChar(auto_follow + 41, 0); -- AutoRun
-        mem.WriteULong(auto_follow + 36, 0x4000000); -- FollowID
-        mem.WriteFloat(auto_follow + 12, 0); -- DirectionX
-        mem.WriteFloat(auto_follow + 16, 0); -- DirectionY
-        mem.WriteFloat(auto_follow + 20, 0); -- DirectionZ
+        ashita.memory.write_uint8(auto_follow + 41, 0); -- AutoRun
+        ashita.memory.write_uint32(auto_follow + 36, 0x4000000); -- FollowID
+        write_float_hack(auto_follow + 12, 0); -- DirectionX
+        write_float_hack(auto_follow + 16, 0); -- DirectionY
+        write_float_hack(auto_follow + 20, 0); -- DirectionZ
     end
 end
 
 ashita.register_event('command', function(cmd, nType)
-    local args = cmd:GetArgs();
+    local args = cmd:args();
 
     if (#args > 0 and args[1] == '/autorun')  then
         if (#args > 1)  then
@@ -56,11 +64,11 @@ ashita.register_event('command', function(cmd, nType)
 end);
 
 ashita.register_event('load', function()
-    local ptr = mem.FindPatternEx('FFXiMain.dll', '8BCFE8????????8B0D????????E8????????8BE885??750CB9', 0, 0);
+    local ptr = ashita.memory.findpattern('FFXiMain.dll', 0, '8BCFE8????????8B0D????????E8????????8BE885??750CB9', 0, 0);
     if (ptr == 0) then
         print('[ERROR] Failed to read ptr.');
     else
-        auto_follow = mem.ReadULong(ptr + 25);
+        auto_follow = ashita.memory.read_uint32(ptr + 25);
     end
 end );
 
